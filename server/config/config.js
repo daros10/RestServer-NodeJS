15405a51eb9c6846a88d00f368b53e1c70b497a2
Daro <daro.wla@hotmail.es>
2020-05-29 22:22:54 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,9 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://daros10:ckwURAz9Q8pEraxH@cluster0-pngyb.mongodb.net/cafe'
+    // urlDB = 'mongodb+srv://daros10:ckwURAz9Q8pEraxH@cluster0-pngyb.mongodb.net/cafe'
+    // Mongo URI es la variable de entorno configura en Heroku
+    urlDB = process.env.MONGO_URI;
 }
 
 // Creo un nombre de env cualquiera y asigno el valor de urlDB
